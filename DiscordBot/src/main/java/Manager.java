5232311d356600fb4e3c50512411421b5e869113
Isand63 <113596832+Isand63@users.noreply.github.com>
2023-02-22 08:23:13 +0300

    Update Manager.java

--- DiscordBot/src/main/java/Manager.java
+++ DiscordBot/src/main/java/Manager.java
@@ -5,7 +5,6 @@ import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,55 +28,59 @@ public class Manager extends ListenerAdapter {
 //            AudioManager audioManager = guild.getAudioManager();
 //            audioManager.openAudioConnection(channel);
 //        }
-        switch (com[0]) {
-            case ("!play"):
-                try {
+
+            switch (com[0]) {
+                case ("!play"):
                     musicManager.scheduler.player.stopTrack();
                     musicManager.scheduler.queue.clear();
                     final String content = event.getMessage().getContentRaw();
                     final List<String> command = Arrays.asList(content.split(" "));
                     Guild guild = event.getGuild();
+                    try {
                     VoiceChannel channel = member.getVoiceState().getChannel().asVoiceChannel();
                     final AudioManager audioManager = event.getGuild().getAudioManager();
                     audioManager.openAudioConnection(channel);
                     PlayerManager.getInstance().loadAndPlay(guild, command.get(1), channel2);
-                }catch (Exception e){
+                }catch (Exception e) {
                     channel2.sendMessage("Вы не в канале").queue();
                 }
-                break;
-            case ("!stop"):
-                musicManager.scheduler.player.stopTrack();
-                musicManager.scheduler.queue.clear();
-                channel2.sendMessage("Трек остановлен").queue();
-                break;
+                    break;
+                case ("!stop"):
+                    musicManager.scheduler.player.stopTrack();
+                    musicManager.scheduler.queue.clear();
+                    channel2.sendMessage("Трек остановлен").queue();
+                    break;
 
-            case ("!skip"):
-                musicManager.scheduler.nextTrack();
-                channel2.sendMessage("Музыка пропущенна").queue();
-                break;
+                case ("!skip"):
+                    musicManager.scheduler.nextTrack();
+                    channel2.sendMessage("Музыка пропущенна").queue();
+                    break;
 
-            case ("!pause"):
-                musicManager.scheduler.player.setPaused(true);
-                channel2.sendMessage("Музыка приостановлена").queue();
-                break;
+                case ("!pause"):
+                    musicManager.scheduler.player.setPaused(true);
+                    channel2.sendMessage("Музыка приостановлена").queue();
+                    break;
 
-            case ("!resume"):
-                musicManager.scheduler.player.setPaused(false);
-                channel2.sendMessage("Музыка возобнавлена").queue();
-                break;
+                case ("!resume"):
+                    musicManager.scheduler.player.setPaused(false);
+                    channel2.sendMessage("Музыка возобнавлена").queue();
+                    break;
 
-            case ("!leave"):
-                musicManager.scheduler.player.stopTrack();
-                musicManager.scheduler.queue.clear();
-                AudioManager aud = event.getGuild().getAudioManager();
-                aud.closeAudioConnection();
-                channel2.sendMessage("Бот покинул чат");
-                break;
+                case ("!leave"):
+                    musicManager.scheduler.player.stopTrack();
+                    musicManager.scheduler.queue.clear();
+                    AudioManager aud = event.getGuild().getAudioManager();
+                    aud.closeAudioConnection();
+                    channel2.sendMessage("Бот покинул чат");
+                    break;
 
-            default:
-                channel2.sendMessage("Неправильная комманда");
-                break;
+                default:
+                    channel2.sendMessage("Неправильная комманда");
+                    break;
+            }
 
+
+        switch (com[0]) {
             case ("!help"):
                 channel2.sendMessage("!play <ссылка> - бот подключится и по ссылке запустит музыку\n" +
                         "!stop - остановит трек и очистит очередь\n" +
@@ -87,5 +90,5 @@ public class Manager extends ListenerAdapter {
                         "!leave - насильно отключит бота если он напрягает").queue();
                 break;
         }
+        }
     }
-}
